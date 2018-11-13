3a10dfc45620dd531df8ee029e68bbc6d8386af1
monirul89 <monirul89@gmail.com>
2018-11-13 17:18:04 +0600

    a

---
+++ src/component/news_list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const NewsList = (props)=>{
+    return (
+        <div>{this.props.donkey }</div>
+    );
+}
+
+export default NewsList;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 
-
+import JSON from './db.json'
 //component
 import Header from './component/header';
+import NewsList from './component/news_list'
 
-const App = ()=>{
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
+
+class  App extends Component{
+
+    state = {
+        news:JSON
+    }
+
+    render(){
+        return (
+            <div>
+                <Header/>
+                <NewsList news={ this.state.news} donkey="Me"/>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App/>,document.querySelector("#root"));
